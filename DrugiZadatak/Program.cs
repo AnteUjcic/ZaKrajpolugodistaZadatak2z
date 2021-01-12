47de3990575657ab11e081f5f65a5d874127c1ac
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-01-12 16:18:11 +0100

    Add files via upload

---
+++ DrugiZadatak/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DrugiZadatak
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int broj;
+
+
+            Console.Write("Upisite br: ");
+            broj = Convert.ToInt32(Console.ReadLine());
+            if (broj % 4 == 0 && broj % 6 != 0) { Console.WriteLine("Broj je dijeliv s 4 ");  }
+            if (broj % 6 == 0 && broj % 4 != 0) { Console.WriteLine("Broj je dijeliv s 6 ");  }
+            if (broj % 4 == 0 && broj % 6 == 0) { Console.WriteLine("Broj je dijeliv s 4 i 6 "); }
+            if (broj % 4 != 0 && broj % 6 != 0) { Console.WriteLine("Broj je nije dijeliv s 4 i 6 "); }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
